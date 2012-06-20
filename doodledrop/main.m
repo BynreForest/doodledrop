2ed142d8abcbf56eac706a4cb090b97d2c00ae8b
Brendan Gaynor <gaynorbrendan@gmail.com>
2012-06-20 13:47:51 -0400

    Initial Commit

---
+++ doodledrop/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  doodledrop
+//
+//  Created by Brendan Gaynor on 6/20/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
